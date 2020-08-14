3d49ebbe70a62822984339f205a3dc43cac7f23d
Сырдыбаев Айдар <aidar_1997_kg@mail.ru>
2020-08-14 17:36:59 +0600

    add UnitOfWork

--- DeadLine9.DAL/Context/UnitOfWork.cs
+++ DeadLine9.DAL/Context/UnitOfWork.cs
@@ -1,6 +1,41 @@
-﻿namespace DeadLine9.DAL.Context
+﻿using System;
+
+namespace DeadLine9.DAL.Context
 {
-    public class UnitOfWork
+    public class UnitOfWork : IDisposable
     {
+        private readonly ApplicationDbContext _context;
+        private bool _disposed;
+
+       
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+
+        }
+
+        #region Disposable
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+                _context.Dispose();
+
+            _disposed = true;
+        }
+
+        ~UnitOfWork()
+        {
+            Dispose(false);
+        }
+        #endregion
     }
 }
