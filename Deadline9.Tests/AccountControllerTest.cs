31e2fc5a568705cffc4326dfd91627e62c37b6c5
aidar aidar <aidarsyrdybaev1998@gmail.com>
2020-09-12 00:04:01 +0600

    Deadline-13

---
+++ Deadline9.Tests/AccountControllerTest.cs
@@ -0,0 +1,146 @@
+﻿using DeadLine9.DAL.Entities;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Deadline9.UI.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using Deadline9.Models;
+
+namespace Deadline9.Tests
+{
+    public class AccountControllerTests
+    {
+        [Fact]
+        public void GetRegisterTest()
+        {
+            var UserManagerMock = new Mock<FakeUserManager>();
+            var SignInManagerMock = new Mock<FakeSignInManager>();
+
+            var AccountController = new AccountController(UserManagerMock.Object, SignInManagerMock.Object);
+
+            var result = AccountController.Register();
+
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void HttpRegisterTest()
+        {
+            var UserManagerMock = new Mock<FakeUserManager>();
+            var SignInManagerMock = new Mock<FakeSignInManager>();
+
+            UserManagerMock.Setup(repo => repo.CreateAsync(null, null)).Returns(UserCreateAsyncMockMethod(null, null));
+            SignInManagerMock.Setup(repo => repo.SignInAsync(null, null, null)).Returns(UserSigninAsyncMockMethod(null, null, null));
+
+            var AccountController = new AccountController(UserManagerMock.Object, SignInManagerMock.Object);
+
+            var Result = AccountController.Register(new UserRegisterModel { Email = "Aidar_1997_kg@mail.ru", Password = "!Number98", PasswordConfirm = "!Number98" });
+
+            Assert.NotNull(Result);
+
+        }
+
+        [Fact]
+        public void GetLoginTest()
+        {
+            var UserManagerMock = new Mock<FakeUserManager>();
+            var SignInManagerMock = new Mock<FakeSignInManager>();
+
+            var AccountController = new AccountController(UserManagerMock.Object, SignInManagerMock.Object);
+
+            var Result = AccountController.Login("url");
+
+            Assert.NotNull(Result);
+
+        }
+
+        [Fact]
+        public void PostLoginTest()
+        {
+            var UserManagerMock = new Mock<FakeUserManager>();
+            var SignInManagerMock = new Mock<FakeSignInManager>();
+
+            SignInManagerMock.Setup(repo => repo.PasswordSignInAsync("UserName", null, false, false)).Returns(PasswordSignInAsyncMockMethod("UserName", null, false, false));
+
+            var AccountController = new AccountController(UserManagerMock.Object, SignInManagerMock.Object);
+
+            var Result = AccountController.Login(new UserLoginModel { Email = "Aidar_1997_kg@mail.ru", Password ="!Number98", RememberMe=false, ReturnUrl=null});
+
+            Assert.NotNull(Result);
+
+
+        }
+
+        [Fact]
+        public void LogoutTest()
+        {
+            var UserManagerMock = new Mock<FakeUserManager>();
+            var SignInManagerMock = new Mock<FakeSignInManager>();
+
+            SignInManagerMock.Setup(repo => repo.SignOutAsync()).Returns(SignOutAsyncMockMethod());
+
+            var AccountController = new AccountController(UserManagerMock.Object, SignInManagerMock.Object);
+
+            var Result = AccountController.Logout();
+
+            Assert.NotNull(Result);
+        }
+
+
+        public async Task<IdentityResult> UserCreateAsyncMockMethod(User user, ClaimsPrincipal claimsPrincipal)
+        {
+            return IdentityResult.Success;
+        }
+
+        public async Task UserSigninAsyncMockMethod (User user, AuthenticationProperties authenticationProperties, string authMethod)
+        { 
+        
+        }
+
+        public async Task<SignInResult> PasswordSignInAsyncMockMethod(string UserName, string Password, bool IsPersistent, bool LockoutOnFailure)
+        {
+            return SignInResult.Success;
+        }
+
+        public async Task SignOutAsyncMockMethod()
+        { 
+        
+        }
+    }
+
+    public class FakeSignInManager : SignInManager<User>
+    {
+        public FakeSignInManager()
+            : base(new FakeUserManager(),
+            new Mock<IHttpContextAccessor>().Object,
+            new Mock<IUserClaimsPrincipalFactory<User>>().Object,
+            new Mock<IOptions<IdentityOptions>>().Object,
+            new Mock<ILogger<SignInManager<User>>>().Object,
+            new Mock<IAuthenticationSchemeProvider>().Object, 
+            new Mock<IUserConfirmation<User>>().Object)
+        { }
+    }
+
+    public class FakeUserManager : UserManager<User>
+    {
+        public FakeUserManager()
+            : base(new Mock<IUserStore<User>>().Object,
+                  new Mock<IOptions<IdentityOptions>>().Object,
+                  new Mock<IPasswordHasher<User>>().Object,
+                  new IUserValidator<User>[0],
+                  new IPasswordValidator<User>[0],
+                  new Mock<ILookupNormalizer>().Object,
+                  new Mock<IdentityErrorDescriber>().Object,
+                  new Mock<IServiceProvider>().Object,
+                  new Mock<ILogger<UserManager<User>>>().Object)
+        { }
+    }
+}
---
+++ Deadline9.Tests/LectureControllerTest.cs
@@ -0,0 +1,82 @@
+﻿using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Moq;
+using Deadline9.BL.Services;
+using Deadline9.UI.Controllers;
+using Deadline9.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Deadline9.Tests
+{
+    public class LectureControllerTest
+    {
+        [Fact]
+        public async Task IndexTest()
+        {
+            //    ILectureService _lectureService { get; }
+            //private IGroupService _groupService { get; }
+            //private ITeacherService _teacherService { get; }
+            //private ILessionService _lessionService { get; }
+
+            var LectureServiceMock = new Mock<ILectureService>();
+            var GroupServiceMock = new Mock<IGroupService>();
+            var TeacherServiceMock = new Mock<ITeacherService>();
+            var LessionServiceMock = new Mock<ILessionService>();
+
+            LectureServiceMock.Setup(repo => repo.GetAll()).Returns(GetLectureIndexModels());
+
+            var LectureController = new LectureController(LectureServiceMock.Object, GroupServiceMock.Object, TeacherServiceMock.Object, LessionServiceMock.Object);
+
+            var Result = await LectureController.Index();
+
+            var ViewResult = Assert.IsType<ViewResult>(Result);
+
+            var Model = Assert.IsAssignableFrom<List<LectureIndexModel>>(ViewResult.Model);
+
+            Assert.Equal(Model.Count, GetLectureIndexModels().Count);
+
+        }
+
+        [Fact]
+
+        public async Task DetailsTest()
+        {
+            var LectureServiceMock = new Mock<ILectureService>();
+            var GroupServiceMock = new Mock<IGroupService>();
+            var TeacherServiceMock = new Mock<ITeacherService>();
+            var LessionServiceMock = new Mock<ILessionService>();
+
+            LectureServiceMock.Setup(repo => repo.GetDetailsModel(1)).Returns(GetDetailsModel(1));
+
+            var LectureController = new LectureController(LectureServiceMock.Object, GroupServiceMock.Object, TeacherServiceMock.Object, LessionServiceMock.Object);
+
+            var Result = await LectureController.Details(1);
+
+            var ViewResult = Assert.IsType<ViewResult>(Result);
+
+            var Model = Assert.IsAssignableFrom<LectureDetailsModel>(ViewResult.Model);
+
+            Assert.Equal(Model.TeacherName, GetDetailsModel(1).TeacherName);
+        }
+
+        private List<LectureIndexModel> GetLectureIndexModels()
+        {
+            return new List<LectureIndexModel> {
+                new LectureIndexModel{Id = 1, LessionName = "Математика-1" },
+                new LectureIndexModel{Id = 2, LessionName = "Математика-2" },
+                new LectureIndexModel{Id = 3, LessionName = "Математика-3" },
+                new LectureIndexModel{Id = 4, LessionName = "Математика-4" },
+                new LectureIndexModel{Id = 5, LessionName = "Математика-5" }
+             };
+        }
+
+        private LectureDetailsModel GetDetailsModel(int? Id)
+        {
+            return new LectureDetailsModel {Id = 1, GroupId=1, GroupName = "Group", LessionId = 1, LessionName="Математика", TeacherId=1, TeacherName="Тичар"};
+        }
+
+    }
+}
