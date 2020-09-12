7c72e8897cd7f75ee69894914938cfd2bdcd4f1c
aidar aidar <aidarsyrdybaev1998@gmail.com>
2020-09-12 18:41:57 +0600

    13DeadlineCommit

--- Deadline9.Tests/LectureControllerTest.cs
+++ Deadline9.Tests/LectureControllerTest.cs
@@ -8,6 +8,8 @@ using Deadline9.UI.Controllers;
 using Deadline9.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using DeadLine9.DAL.Entities;
 
 namespace Deadline9.Tests
 {
@@ -62,6 +64,107 @@ namespace Deadline9.Tests
             Assert.Equal(Model.TeacherName, GetDetailsModel(1).TeacherName);
         }
 
+
+        [Fact]
+
+        public async Task GetCreateTest()
+        {
+            var LectureServiceMock = new Mock<ILectureService>();
+            var GroupServiceMock = new Mock<IGroupService>();
+            var TeacherServiceMock = new Mock<ITeacherService>();
+            var LessionServiceMock = new Mock<ILessionService>();
+
+            GroupServiceMock.Setup(repo => repo.GetSelectListItems()).Returns(GetGroupSelectListItems());
+            TeacherServiceMock.Setup(repo => repo.GetSelectListItems()).Returns(GetTeacherSelectListItems());
+            LessionServiceMock.Setup(repo => repo.GetSelectListItems()).Returns(GetLessionSelectListItems());
+
+            var LectureController = new LectureController(LectureServiceMock.Object, GroupServiceMock.Object, TeacherServiceMock.Object, LessionServiceMock.Object);
+
+            var Result = LectureController.Create();
+
+            Assert.NotNull(Result);
+
+        }
+        [Fact]
+        public async Task PostCreateTest()
+        {
+            var LectureServiceMock = new Mock<ILectureService>();
+            var GroupServiceMock = new Mock<IGroupService>();
+            var TeacherServiceMock = new Mock<ITeacherService>();
+            var LessionServiceMock = new Mock<ILessionService>();
+
+            LectureServiceMock.Setup(repo => repo.Create(null)).Callback((LectureCreateModel model) => { } );
+
+            var LectureController = new LectureController(LectureServiceMock.Object, GroupServiceMock.Object, TeacherServiceMock.Object, LessionServiceMock.Object);
+
+            var Result = await LectureController.Create(null);
+
+            Assert.NotNull(Result);
+        }
+
+        [Fact]
+        public async Task GetEditTest()
+        {
+            var LectureServiceMock = new Mock<ILectureService>();
+            var GroupServiceMock = new Mock<IGroupService>();
+            var TeacherServiceMock = new Mock<ITeacherService>();
+            var LessionServiceMock = new Mock<ILessionService>();
+
+            GroupServiceMock.Setup(repo => repo.GetSelectListItems()).Returns(GetGroupSelectListItems());
+            TeacherServiceMock.Setup(repo => repo.GetSelectListItems()).Returns(GetTeacherSelectListItems());
+            LessionServiceMock.Setup(repo => repo.GetSelectListItems()).Returns(GetLessionSelectListItems());
+            LectureServiceMock.Setup(repo => repo.GetEditModel(1)).Returns(LectureGetEditModelMockMethod(1));
+
+            var LectureController = new LectureController(LectureServiceMock.Object, GroupServiceMock.Object, TeacherServiceMock.Object, LessionServiceMock.Object);
+
+            var Result = await LectureController.Edit(1);
+
+            var ViewResult = Assert.IsType<ViewResult>(Result);
+
+            var Model = Assert.IsAssignableFrom<LectureEditModel>(ViewResult.Model);
+
+            Assert.Equal(Model.Id, LectureGetEditModelMockMethod(1).Id);
+
+        }
+
+        [Fact]
+        public async Task PostEditTest()
+        {
+            var LectureServiceMock = new Mock<ILectureService>();
+            var GroupServiceMock = new Mock<IGroupService>();
+            var TeacherServiceMock = new Mock<ITeacherService>();
+            var LessionServiceMock = new Mock<ILessionService>();
+
+            LectureServiceMock.Setup(repo => repo.Edit(new LectureEditModel { Id = 1, GroupId = 1, LessionId = 1, TeacherId = 1 })).Callback((LectureEditModel model) => { });
+
+            var LectureController = new LectureController(LectureServiceMock.Object, GroupServiceMock.Object, TeacherServiceMock.Object, LessionServiceMock.Object);
+
+            var Result = await LectureController.Edit(new LectureEditModel { Id = 1, GroupId = 1, LessionId = 1, TeacherId = 1 });
+
+            Assert.NotNull(Result);
+
+
+        }
+
+        [Fact]
+        public async Task DeleteTest()
+        {
+            var LectureServiceMock = new Mock<ILectureService>();
+            var GroupServiceMock = new Mock<IGroupService>();
+            var TeacherServiceMock = new Mock<ITeacherService>();
+            var LessionServiceMock = new Mock<ILessionService>();
+
+            LectureServiceMock.Setup(repo => repo.Delete(1)).Callback((int Id) => { });
+
+            var LectureController = new LectureController(LectureServiceMock.Object, GroupServiceMock.Object, TeacherServiceMock.Object, LessionServiceMock.Object);
+
+            var Result = LectureController.Delete(1);
+
+            Assert.NotNull(Result);
+        }
+        
+
+
         private List<LectureIndexModel> GetLectureIndexModels()
         {
             return new List<LectureIndexModel> {
@@ -73,9 +176,54 @@ namespace Deadline9.Tests
              };
         }
 
+
         private LectureDetailsModel GetDetailsModel(int? Id)
         {
             return new LectureDetailsModel {Id = 1, GroupId=1, GroupName = "Group", LessionId = 1, LessionName="Математика", TeacherId=1, TeacherName="Тичар"};
+        }
+
+        private SelectList GetTeacherSelectListItems()
+        {
+            var Teachers = new List<Teacher>()
+            {
+                new Teacher{ Id=1, Name="TeacherName1"},
+                new Teacher{ Id=2, Name="TeacherName2"},
+                new Teacher{ Id=3, Name="TeacherName3"},
+                new Teacher{ Id=4, Name="TeacherName4"}
+            };
+
+            return new SelectList (Teachers ,"Id", "Name" );
+        }
+
+        private SelectList GetGroupSelectListItems()
+        {
+            var Groups = new List<Group>()
+            {
+                new Group{ Id = 1, Name ="Group1"},
+                new Group{ Id = 1, Name ="Group2"},
+                new Group{ Id = 1, Name ="Group3"},
+                new Group{ Id = 1, Name ="Group4"}
+            };
+
+            return new SelectList(Groups, "Id", "Name");
+        }
+
+        private SelectList GetLessionSelectListItems()
+        {
+            var Lessions = new List<Lession>()
+            {
+                new Lession{Id = 1, Name="Lession1"},
+                new Lession{Id = 2, Name="Lession2"},
+                new Lession{Id = 3, Name="Lession3"},
+                new Lession{Id = 4, Name="Lession4"}
+            };
+
+            return new SelectList(Lessions, "Id", "Name");
+        }
+
+        private LectureEditModel LectureGetEditModelMockMethod(int Id)
+        {
+            return new LectureEditModel { Id = 1, GroupId = 1, LessionId = 1, TeacherId = 1 };
         }
 
     }
